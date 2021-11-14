459fd62098e492fd923374cf0e7a76de7054c2b3
LIN GUAN-YU <kib7410@gmail.com>
2021-11-14 15:50:44 +0800

    '第二個路由:新增任務與資料驗證'

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,21 @@ Route::get('/', function () {
 });
 // 增加新的任務
 Route::post('/task', function (Request $request){
-    //
+    // 驗證輸入
+    $validator=Validator::make($request->all(),[
+        'name'=>'required|max:255'
+    ]);
+    if ($validator->fails()){
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    //建立該任務
+    //新增任務存入DB的程式碼
+    $task=new Task();
+    $task->name=$request->name;
+    $task->save();
+    return redirect('/');
 });
 // 刪除任務
 Route::delete('/task/{task}', function (Task $task){
